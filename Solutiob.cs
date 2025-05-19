186b2f2abbef7a266a3b1439e4e32655f006e813
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-05-19 22:26:09 +0530

    Create Solution.cs

---
+++ Solutiob.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Test
+{
+	public static void Main()
+	{
+		int cases = Convert.ToInt32(Console.ReadLine());
+		for(int c = 0; c < cases; c++){
+		    string[] inputs = Console.ReadLine().Split();
+            int a = int.Parse(inputs[0]);
+            int b = int.Parse(inputs[1]);
+            
+            a = a*b;
+            Console.WriteLine(a/100);
+		}
+	}
+}
